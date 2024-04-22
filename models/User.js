4f8e6cd6e81d036c1773379bc2503ecc87cf5f81
agpadilla40 <agpadilla40@gmail.com>
2024-04-21 22:29:51 -0600

    modelos, controladores y rutas

---
+++ models/User.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+    },
+    shippingAddress: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN', 'USER'],
+        default: "USER",
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+export default mongoose.model("User", userSchema);
---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from '../controllers/userController.js';
+import { authValidator } from '../middlewares/authValidator.js';
+import { isAdmin } from '../middlewares/isAdmin.js';
+
+const userRoutes = express.Router();
+
+userRoutes.route('/').post(createUser).get(authValidator, isAdmin, getAllUsers);
+userRoutes.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+export default userRoutes;
